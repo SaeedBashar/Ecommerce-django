8660b133f61765f28668c3ccf47e5d09e65cfb6c
Bashar <Basharsaeed200@gmail.com>
2023-09-13 00:08:35 +0000

    Removed groups model and changed one zip code input control

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from django.contrib.auth.models import Group  # Import the Group model
+
+# Unregister the Group model from the admin site
+admin.site.unregister(Group)
 # Register your models here.
